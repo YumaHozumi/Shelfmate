cebbfced4dddb9a9e06c9e1526d5597afa1c9497
YumaHozumi <cpucmptrpython72@gmail.com>
2023-07-22 00:24:45 +0900

    basicコンポーネントにLabelを分離

--- frontend/src/containers/Form/RegisterStep1.vue
+++ frontend/src/containers/Form/RegisterStep1.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SubmitButton from "@/basic//Login/SubmitButton.vue";
+import Label from "@/basic/Label.vue";
 
 interface Emits {
     (event: "submitButton"): void;
@@ -24,7 +25,7 @@ const submitButton = (): void => {
         </v-row>
         <v-row class="mx-1">
             <v-col cols="12">
-                <div class="label mb-3">メールアドレス</div>
+                <Label text="メールアドレス" class="mb-3"></Label>
                 <input type="text" class="input-form ps-2">
             </v-col>
             <v-col cols="12">
@@ -40,21 +41,5 @@ const submitButton = (): void => {
     background-color: white;
     border: 1px solid rgb(206, 205, 205);
     width: 100%;
-}
-
-.label {
-    font-weight: bold;
-    color: #333;
-    margin-left: 12px;
-
-    &::before {
-        content: "";
-        position: absolute;
-        margin-left: -12px;
-        margin-top: -1px;
-        width: 6px;
-        height: 22px;
-        background: #dedede;
-    }
 }
 </style>
