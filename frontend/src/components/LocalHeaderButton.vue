b93d18e05ea84b4e7d9ad9c085f8bcfbcfbc14a8
YumaHozumi <cpucmptrpython72@gmail.com>
2023-07-24 12:19:26 +0900

    v-btnを分離

---
+++ frontend/src/components/LocalHeaderButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface Props {
+    text: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <v-btn>{{ text }}</v-btn>
+</template>
--- frontend/src/containers/LocalHeader.vue
+++ frontend/src/containers/LocalHeader.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+import LocalHeaderButton from '@/components/LocalHeaderButton.vue';
+</script>
+
 <template>
     <v-app-bar color="green" elevation="0" class="fix">
-        <v-btn class="custom-button">hoge</v-btn>
-        <v-btn class="custom-button">hogfffffffffffffffffe</v-btn>
-        <v-btn class="custom-button">hoge</v-btn>
+        <LocalHeaderButton text="hoge" class="custom-button"></LocalHeaderButton>
+        <LocalHeaderButton text="hogeaaaaaaaaaaaaaaaaaa" class="custom-button"></LocalHeaderButton>
+        <LocalHeaderButton text="hoge" class="custom-button"></LocalHeaderButton>
          
     </v-app-bar>
 
@@ -10,7 +14,7 @@
 
 <style scoped lang="scss">
 .fix {
-    height: 30px;
+    height: 33px;
 
     .custom-button {
         margin-bottom: 35px;
