a10ad208e35b3fd43c941cdef990f2d9b2642b4c
YumaHozumi <cpucmptrpython72@gmail.com>
2023-07-25 19:38:40 +0900

    ナビゲーションを追加

---
+++ frontend/src/views/SeriesView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SeriesHeader from '@/containers/SeriesHeader.vue';
+import router from '@/router'
+
+const onNavigate = (name: string): void => {
+  router.push({name: name});
+}
+</script>
+
+<template>
+    <SeriesHeader @navigate="onNavigate"></SeriesHeader>
+</template>
