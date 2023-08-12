ed7d81113f5fa6419407a45afb37ae8b86ea692b
YumaHozumi <cpucmptrpython72@gmail.com>
2023-08-12 11:55:16 +0900

    ローディングファイル追加しわすれ

---
+++ frontend/src/containers/LoadingContainer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Props {
+    isLoading: boolean
+}
+
+defineProps<Props>();
+// ローディングフラグ
+</script>
+
+<template>
+    <div class="loading" v-if="isLoading">
+        <v-progress-circular
+        indeterminate
+        :size="100"
+        color="green"
+        ></v-progress-circular>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
