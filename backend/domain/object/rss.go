9406ca7a9642517372f0e3ae0c36630d629ab0fa
YumaHozumi <cpucmptrpython72@gmail.com>
2023-08-25 20:44:44 +0900

    ISBNの検索結果取得できるように

--- backend/domain/object/rss.go
+++ backend/domain/object/rss.go
@@ -1,6 +1,11 @@
 package object
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 type RSS struct {
 	Channel Channel `xml:"channel" json:"result"`
@@ -15,7 +20,7 @@ type Item struct {
 	Detail         string `json:"detail"` // 読み込むのはカスタム関数で
 	Author         string `xml:"author" json:"author"`
 	PublicDate     string `xml:"pubDate" json:"public_date"`
-	ISBNIdentifier string `json:"isbn"`
+	ISBNIdentifier int64  `json:"isbn"`
 	OrderNumber    int    `xml:"volume" json:"order_number"`
 	ImageURL       string `json:"image_url"`
 }
@@ -48,14 +53,27 @@ func (item *Item) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 			case "identifier":
 				for _, attr := range t.Attr {
 					if attr.Name.Local == "type" && attr.Value == "dcndl:ISBN" {
-						var isbn string
+						var isbn int64
 						if err := d.DecodeElement(&isbn, &t); err != nil {
 							return err
 						}
 						item.ISBNIdentifier = isbn
-						if isbn != "" {
-							picImage := "https://iss.ndl.go.jp/thumbnail/" + isbn
-							item.ImageURL = picImage
+
+						if strISBN := strconv.FormatInt(isbn, 10); strISBN != "" {
+							picImage := "https://iss.ndl.go.jp/thumbnail/" + strISBN
+
+							resp, err := http.Head(picImage)
+							if err != nil {
+								return err
+							}
+							defer resp.Body.Close()
+							fmt.Println(resp)
+
+							if resp.StatusCode == http.StatusNotFound {
+								item.ImageURL = ""
+							} else {
+								item.ImageURL = picImage
+							}
 						}
 					}
 				}
