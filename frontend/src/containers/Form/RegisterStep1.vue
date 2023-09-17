fee8273c54f18cca178524da761a4c47c7b6250d
YumaHozumi <cpucmptrpython72@gmail.com>
2023-09-17 12:03:54 +0900

    URL更新

--- frontend/src/containers/Form/RegisterStep1.vue
+++ frontend/src/containers/Form/RegisterStep1.vue
@@ -37,7 +37,7 @@ const submitButton = async () => {
     }
     const cred = await createUserWithEmailAndPassword(firebaseAuth, email.value, password.value)
     const actionCodeSettings = {
-      url: 'http://localhost:10400/login', // replace this with the URL of your top page
+      url: 'http://shelfmate.hzmintech.com/login', // replace this with the URL of your top page
       handleCodeInApp: true
     }
     await sendEmailVerification(cred.user, actionCodeSettings)
