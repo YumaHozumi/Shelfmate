c66736bac5d5fb94840c16eb9173cd3ef9e270e9
YumaHozumi <cpucmptrpython72@gmail.com>
2024-12-23 16:47:16 +0900

    chore: @storybook/vue3-viteをdevDependenciesに追加し、Vuetifyを設定

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,29 @@
+import type { Preview } from "@storybook/vue3";
+import { setup } from '@storybook/vue3'
+import vuetify from "../src/plugins/vuetify";
+
+setup((app) => {
+  if (app) {
+    app.use(vuetify)
+  }
+})
+
+export const decorators = [
+  (story: any) => ({
+    components: { story },
+    template: '<v-app><story /></v-app>',
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
